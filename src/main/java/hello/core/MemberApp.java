6322514c2bdbbb9b1bc7859fac21040cc139ebcb
sunwooKim <frod5@kakao.com>
2023-03-24 16:10:29 +0900

    스프링 핵심 원리 - 기본편 AppConfig(구성 객체)로 의존관계를 주입 -> 스프링 코드로 전환 (ApplicationContext)

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -1,12 +1,18 @@
 package hello.core;
 
 import hello.core.member.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        //@Bean으로 선언된 것들을 Spring container로 사용할 수 있도록 해준다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
 
         Member member = new Member(1L,"memberA", Grade.VIP);
 
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -5,13 +5,22 @@ import hello.core.member.*;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+//
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        //ApplicationContext를 스프링 컨테이너라고 한다.
+        //@Bean으로 선언된 것들을 컨테이너에 등록.
+        //기본적으로 메소드가 빈 이름.
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService",MemberService.class);
+        OrderService orderService = ac.getBean("orderService",OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId,"memberA", Grade.VIP);
