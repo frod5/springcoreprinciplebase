2efc7930ce390b4c97b032e26075094e4bc94145
sunwooKim <frod5@kakao.com>
2023-03-24 15:20:39 +0900

    스프링 핵심 원리 - 기본편 AppConfig를 이용해 DIP 원칙을 지키도록 리팩토링. 외부에서 해당 객체를 주입하도록(의존관계 주입)

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -1,17 +1,17 @@
 package hello.core;
 
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.*;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId,"memberA", Grade.VIP);
@@ -21,5 +21,4 @@ public class OrderApp {
 
         System.out.println("order :: >>>> "+order.toString());
     }
-
 }
