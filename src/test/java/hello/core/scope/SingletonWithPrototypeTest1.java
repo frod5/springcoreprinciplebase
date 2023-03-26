306b5bddd21b2a711ec918afbe8bbd24a5397275
sunwooKim <frod5@kakao.com>
2023-03-26 17:48:25 +0900

    스프링 핵심 원리 - 기본편 싱글톤, 프로토타입 함께 사용할 경우 문제점 Provider를 사용하여 해결. Spring ObjectProvider, 자바 표준 Provider

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,11 +2,14 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 public class SingletonWithPrototypeTest1 {
 
@@ -31,25 +34,25 @@ public class SingletonWithPrototypeTest1 {
         int count1 = clientBean1.logic();
         Assertions.assertThat(count1).isEqualTo(1);
 
-        ClientBean clientBean2 = ac.getBean(ClientBean.class);  //프로토타입 빈이 이미 위에 한번 생성이 되어서 새로 생성 된것이 아니다.
-        int count2 = clientBean2.logic();
-        Assertions.assertThat(count2).isEqualTo(2);
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();                   //DL을 사용하여 프로토타입은 매번 생성되서 생성된 것을 찾아 주입하여 새로운객체이다.
+        Assertions.assertThat(count2).isEqualTo(1);
 
 //        스프링은 일반적으로 싱글톤 빈을 사용하므로, 싱글톤 빈이 프로토타입 빈을 사용하게 된다.
 //        그런데 싱글톤 빈은 생성 시점에만 의존관계 주입을 받기 때문에,
 //        프로토타입 빈이 새로 생성되기는 하지만, 싱글톤 빈과 함께 계속 유지되는 것이 문제다.
 
+        //DL을 사용하여 문제점 해결.
+
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;  //생성 시점에 주입
-
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider; // 자바 Provider 스프링에 의존하지 않는다. (자바표준)
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
