72ca6c5f1f51e40dae3fc77a7ecb3b3f9c1be012
sunwooKim <frod5@kakao.com>
2023-03-26 15:26:24 +0900

    스프링 핵심 원리 - 기본편 @Autowired, @Qualifier , @Primary. 같은 빈 조회 됐을 경우 처리 방법 확인. 주석 확안.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,6 +6,7 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,8 +15,10 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//    RateDiscountPolicy에 @Primary를 선언하면 중복되는 빈이 있는경우 우선순위로 가져간다.
+//    @Primary와 @Qualifier가 있는 경우 에는 Qualifier가 우선순위가 높다.
+//    @Autowired 같은 타입의 빈이 있는 경우 ex) DiscountPolicy가 rateDiscountPolicy, fixDiscountPolicy 있는 경우 오류가 나는데, DiscountPolicy rateDiscountPolicy로 선언하면 필드 이름을 보고 주입하여 준다
+//    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("fixDiscountPolicy") DiscountPolicy discountPolicy) { // @Qualifier("fixDiscountPolicy")로 선언 되어 있는 빈을 찾아 주입하여 준다.
 //        this.memberRepository = memberRepository;
 //        this.discountPolicy = discountPolicy;
 //    }
